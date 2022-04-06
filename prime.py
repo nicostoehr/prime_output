b62a8eebc837c88d2f60eeb788cca7ec4d4b3bd1
nicostoehr <58736331+nicostoehr@users.noreply.github.com>
2022-04-06 16:48:44 +0200

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,18 +1,20 @@
 from time import perf_counter, sleep
 
+
 def isPrime(n):
-    if (n <= 1):
+    if n <= 1:
         return False
-    elif (n <= 3):
+    elif n <= 3:
         return True
-    if (n % 2 == 0 or n % 3 == 0):
+    if n % 2 == 0 or n % 3 == 0:
         return False
     i = 5
-    while (i * i <= n):
-        if (n % i == 0 or n % (i + 2) == 0):
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
             return False
-        i = i + 6
+        i += 6
     return True
+
 
 file = open("prime numbers.txt", "w")
 
@@ -21,24 +23,27 @@ primeCount = 0
 start = perf_counter()
 
 firstline = True
-for i in range(2,max):
+for i in range(2, max):
     if isPrime(i):
         if firstline:
             file.write(str(i))
             firstline = False
-            primeCount+=1
+            primeCount += 1
         else:
-            file.write(",\n"+str(i))
-            primeCount+=1
+            file.write(",\n" + str(i))
+            primeCount += 1
 
 end = perf_counter()
 
-file.write(".\nInsgesamt ausgegebene Primzahlen: "+str(primeCount)+" oder "+str(primeCount/max*100)+"% der Zahlen.")
-file.write("\nBenötigte Zeit: "+ str(end-start)+ " Sekunden oder "+str(primeCount/(end-start)) + " Primzahlen pro Sekunde.")
+file.write(".\nInsgesamt ausgegebene Primzahlen: " + str(primeCount) + " oder " + str(
+    primeCount / max * 100) + "% der Zahlen.")
+file.write("\nBenötigte Zeit: " + str(end - start) + " Sekunden oder " + str(
+    primeCount / (end - start)) + " Primzahlen pro Sekunde.")
 file.close()
 
-print("Insgesamt ausgegebene Primzahlen: "+str(primeCount)+" oder "+str(primeCount/max*100)+"% der Zahlen.")
-print("Benötigte Zeit: "+ str(end-start)+ " Sekunden oder "+str(primeCount/(end-start)) + " Primzahlen pro Sekunde.")
-print(start)
-print(end)
+print(
+    "Insgesamt ausgegebene Primzahlen: " + str(primeCount) + " oder %.2f" % (primeCount / max * 100) + "% der Zahlen.")
+print("Benötigte Zeit: %.2f" % (end - start) + " Sekunden oder %.2f" % (
+            primeCount / (end - start)) + " Primzahlen pro Sekunde.")
+
 sleep(15)
